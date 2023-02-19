fa4d5dfe8d9c1e5038f92a474aa494cdfb765555
jessicakay <52629990+jessicakay@users.noreply.github.com>
2023-02-18 23:19:00 -0500

    building out dark more

--- plots/darkmode.R
+++ plots/darkmode.R
@@ -55,8 +55,55 @@ grid.arrange(
           panel.background = element_rect("black"),
           legend.box.background = element_rect("black"), plot.background = element_rect("black",colour = "black"))+
     facet_grid(region~keyword)
-, heights=c(1.5,2))
+, ncol=2)
 
+
+{
+  grid.arrange(
+    ds %>% group_by(the_day,region,keyword) %>% mutate(ct=n()) %>%
+      filter(region!="all regions") %>%
+      ggplot()+
+      geom_line(aes(x=the_day,y=ct,color=keyword, colour="daily"))+
+      geom_point(aes(x=the_day,y=ct,color=keyword, colour="daily"))+
+      labs(title = "Articles about trans people in US + UK news media",
+           subtitle = cappy,
+           caption="github.com/jessicakay/gayagenda | @jessdkant")+
+      xlab(element_blank())+
+      ylab("number of articles")+
+      facet_grid(region~.)+
+      theme_dark()+
+      theme(plot.background=element_rect("black", colour = "black"),panel.grid = element_line("black"),  
+            panel.background = element_rect("black"),
+            legend.background = element_rect("black"),
+            legend.box.background = element_rect("black"),legend.key = element_rect("black"),
+            text = element_text(colour = "white"),
+            legend.position = "bottom")+
+      scale_color_brewer(palette = "Spectral")
+    ,
+    
+    ds %>% group_by(the_day,region,keyword) %>% mutate(ct=n()) %>% 
+      filter(region!="all regions") %>%
+      ggplot()+
+      geom_line(aes(x=the_day,y=ct,color=topic, colour="daily"),alpha = 0.1,position="dodge")+
+      geom_point(aes(x=the_day,y=ct,color=topic, colour="daily",size=ct,alpha = 0.05))+
+      labs(title = "Key topics in article excerpt, stratified by search term & region",
+           subtitle = "color indicates most frequently discussed topic",
+           caption=paste("updated",Sys.time()))+
+      xlab(element_blank())+
+      ylab("number of articles")+
+      theme_dark()+
+      scale_size_continuous(guide = "none")+
+      scale_alpha_continuous(guide="none")+
+      scale_color_brewer(palette = "Spectral")+
+      theme(text=element_text(colour="white"),
+            legend.position = "bottom",legend.background = element_rect("black"),
+            panel.grid.minor = element_line(linetype = "dotted"),
+            panel.grid.major = element_line(linetype = "dotted"), legend.key = element_rect("black"),
+            panel.background = element_rect("black"),
+            legend.box.background = element_rect("black"), plot.background = element_rect("black",colour = "black"))+
+      facet_grid(region~keyword)
+    , ncol=2, heights=c(1.5,2))
+}
 
 
 # generate dark mode table for states 
