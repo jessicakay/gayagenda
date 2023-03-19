42305472cad66870863dba3a50f089e85b274d61
jessicakay <52629990+jessicakay@users.noreply.github.com>
2023-03-19 13:02:49 -0400

    Update extremism_watch.R

--- plots/extremism_watch.R
+++ plots/extremism_watch.R
@@ -2,7 +2,7 @@
 
 exfresh<-function(arg="ex"){
   if(arg=="ex"){
-    googledrive::drive_find(pattern = "rhetoric_tracker",verbose = TRUE ) ->> ext_sheet 
+    googledrive::drive_find(pattern = "rhetorictracker",verbose = TRUE ) ->> ext_sheet 
     exdat <<- NULL
     for(i in 1:dim(ext_sheet)[1]){
       if(i==1){googlesheets4::read_sheet(ext_sheet$id[i]) ->> exdat
@@ -13,3 +13,9 @@ exfresh<-function(arg="ex"){
   }
 }
 exfresh()
+
+exds %>% mutate(theday=str_extract(EntryPublished,pattern = "[a-zA-Z]+\\s[0-9]+\\,\\s20[0-9]+")) %>%
+  mutate(the_day=as.Date(mdy(theday))) ->> exds
+substring(str_extract(exds$EntryURL, pattern="https:\\/\\/?[a-z]+.[a-zA-Z0-9]+?.?[a-z]+/"), first=9) -> exds$pullURL
+
+table(exds$pullURL) %>% View()
