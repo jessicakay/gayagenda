662608060c7618c51a3cc2de49db20d9c5db9307
jessicakay <52629990+jessicakay@users.noreply.github.com>
2023-03-19 10:49:26 -0400

    Update extremism_watch.R

--- plots/extremism_watch.R
+++ plots/extremism_watch.R
@@ -1,2 +1,15 @@
 # 
 
+exfresh<-function(arg="ex"){
+  if(arg=="ex"){
+    googledrive::drive_find(pattern = "rhetoric_tracker",verbose = TRUE ) ->> ext_sheet 
+    exdat <<- NULL
+    for(i in 1:dim(ext_sheet)[1]){
+      if(i==1){googlesheets4::read_sheet(ext_sheet$id[i]) ->> exdat
+      }else{rbind(dat, googlesheets4::read_sheet(ext_sheet$id[i])) -> exdat }
+       exdat -> exds ; exdat ->> exds ; assign("exds",exds,envir = .GlobalEnv)}
+    exds %>% group_by(EntryTitle) %>% as_tibble(as.data.frame()) ->> exds
+  assign("exds",exds,envir = .GlobalEnv)
+  }
+}
+exfresh()
