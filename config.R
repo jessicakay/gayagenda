e3cdebc66abab9545517cee3251f8b3eb0c359d7
jessicakay <52629990+jessicakay@users.noreply.github.com>
2024-03-02 17:07:46 -0500

    Update config.R

--- config.R
+++ config.R
@@ -2,8 +2,7 @@
 # gayagenda config files
 # jessica kant, 2024 
 
-# only to update R base
-
+# package list
 
 c("ggplot2",
   "dplyr",
@@ -13,19 +12,19 @@ c("ggplot2",
   "gridExtra",
   "googlesheets4") -> libs
 
-{
- 
- if(version$major==4 && version$minor==3.3){
-  cat("\n",version$version.string," already installed and loaded.\n\n")}else{
-    if("installr" %in% installed.packages()==FALSE){
-      install.packages("installr")} else {installr::install.R()}}
+# run me if on a new/different machine <3
 
- cat("checking package(s):\n\n",
-    paste(libs[which(as.vector(libs %in% as.data.frame(installed.packages())$Package) == TRUE)]," already installed\n"),
-    "\n",libs[which(as.vector(libs %in% as.data.frame(installed.packages())$Package) == FALSE)]," not found")
+   if(version$major==4 && version$minor==3.3){
+    cat("\n",version$version.string," already installed and loaded.\n\n")}else{
+      if("installr" %in% installed.packages()==FALSE){
+        install.packages("installr")} else {installr::install.R()}}
+  
+   cat("checking package(s):\n\n",
+      paste(libs[which(as.vector(libs %in% as.data.frame(installed.packages())$Package) == TRUE)]," already installed\n"),
+      "\n",libs[which(as.vector(libs %in% as.data.frame(installed.packages())$Package) == FALSE)]," not found")
+  
+    install.packages(libs[which(as.vector(c(libs) %in% as.data.frame(installed.packages())$Package) == FALSE)])
 
- install.packages(libs[which(as.vector(c(libs) %in% as.data.frame(installed.packages())$Package) == FALSE)])
- 
-}
- 
-
+# load all packages
+    
+sapply(libs, require,character.only = TRUE)
