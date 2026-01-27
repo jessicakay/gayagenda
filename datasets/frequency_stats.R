d9c8437455dc877c101a995e17f9add2b2accd3b
jess <jkant@bu.edu>
2026-01-26 21:35:59 -0500

    lubridate

--- datasets/frequency_stats.R
+++ datasets/frequency_stats.R
@@ -5,13 +5,27 @@ ds %>%
   filter(keyword=="transgender") %>% 
   filter(region=="all regions") %>%
   group_by(EntryURL) -> cleaned_ds
+
 View(cleaned_ds)
 
 refresh(arg = "ex")
+
+exds %>% 
+  mutate(the_day=as.Date(mdy(str_extract(EntryPublished,pattern = "[a-zA-Z]+\\s[0-9]+\\,\\s20[0-9]+")))) ->> exds
+  substring(str_extract(exds$EntryURL, pattern="https:\\/\\/?[a-z]+.[a-zA-Z0-9]+?.?[a-z]+/"), first=9) -> exds$pullURL
+
 exds %>% 
   filter(keyword!="woke ideology") %>%
   mutate(month=month(lubridate::as_date(the_day))) %>% 
-  mutate(year = year(lubridate::as_date(the_day))) %>% 
-  group_by(keyword,year) %>%
-  arrange(desc(the_day)) %>%
-  summarise(n=n())
+  mutate(year = year(lubridate::as_date(the_day))) %>%
+  mutate(quarter = quarter(lubridate::as_date(the_day))) -> exds_export
+  
+exds_export -> exds 
+
+exds %>%
+  group_by(keyword,quarter,year) %>%
+  arrange(desc(year)) %>%
+  summarise(n=n()) 
+
+View(exds)
+
