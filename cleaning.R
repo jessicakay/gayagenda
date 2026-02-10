e17cfc4234d01ea832cd1ee93c8840127931401c
jess <jkant@bu.edu>
2026-02-10 18:12:04 -0500

    merged and cleaned

--- cleaning.R
+++ cleaning.R
@@ -1,8 +1,6 @@
 # fixed TLD grabber
 # jessdkant.bsky.social
 
-# read.csv("~/gayagenda/datasets/Jan2026.csv") -> ds
-
 { library(ggplot2)
   library(dplyr)
   library(stringr)
@@ -10,10 +8,8 @@
   library(paletteer)
   library(gridExtra)}
 
-# refresh(arg = "ex")
-
-read.csv("~/gayagenda/datasets/ex_kws.csv") -> exds
-read.csv("~/gayagenda/datasets/Jan2026.csv") -> ds
+  read.csv("~/gayagenda/datasets/ex_kws.csv") -> exds
+  read.csv("~/gayagenda/datasets/Jan2026.csv") -> ds
 
   # newest merge and clean code
   exds[which(exds$keyword!="woke ideology"),] -> exds
@@ -25,36 +21,23 @@ read.csv("~/gayagenda/datasets/Jan2026.csv") -> ds
 
   # if using refresh(arg="ex"): select(exds,c(names(ds))) -> exds
   union(ds,exds) -> mega_ds
+  names(mega_ds)
 
-  c("dailysignal.com",
+  { c("dailysignal.com",
     "christanpost.com",
     "washingtonexaminer.com",
     "dailywire.com",
     "foxnews.com",
     "breitbart.com") -> six_sources
 
-   c( "msn.com",
-      "yahoo.com") -> portals
+   c( "msn.com","yahoo.com") -> portals
 
    c( "youtube.com",
       "x.com",
       "instagram.com",
       "substack",
-      "facebook.com") ->socials
-
-   # top 10 / top 20
-
-    # data cleaning
-     mega_ds[which(mega_ds$keyword != "transgenderism"),] -> mega_ds
-     mega_ds[which(mega_ds$keyword!="gender confusion"),] -> mega_ds
-     mega_ds[which(mega_ds$year!="2026"),] -> mega_ds
-
-     mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"yahoo"))] <- "yahoo.com"
-     mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"twitter.com"))] <- "x.com"
-     mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"thetimes.com"))] <- "thetimes.co.uk"
-     mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"bbc.com"))] <- "bbc.co.uk"
-
-
+      "facebook.com") ->socials }
+  
 mega_ds %>%
   mutate(the_day=as.Date(mdy(str_extract(
     EntryPublished,pattern = "[a-zA-Z]+\\s[0-9]+\\,\\s20[0-9]+")))) %>%
@@ -70,16 +53,14 @@ mega_ds %>%
                  str_remove(EntryURL,"www."),
                  pattern="http?s:\\/\\/[a-z0-9A-Z]+[a-z0-9A-Z.-]+/" ),
                "http?s:\\/\\/"),"/")) -> mega_ds
+  
+  # data cleaning
+  mega_ds[which(mega_ds$keyword != "transgenderism"),] -> mega_ds
+  mega_ds[which(mega_ds$keyword!="gender confusion"),] -> mega_ds
+  mega_ds[which(mega_ds$year!="2026"),] -> mega_ds
+  
+  mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"yahoo"))] <- "yahoo.com"
+  mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"twitter.com"))] <- "x.com"
+  mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"thetimes.com"))] <- "thetimes.co.uk"
+  mega_ds$pullURL[which(str_detect(mega_ds$EntryURL,"bbc.com"))] <- "bbc.co.uk"
 
-  #  ds[which(ds$pullURL=="news.yahoo.com"),]      -> y_a ; dim(a)
-  #  ds[which(ds$pullURL=="yahoo.com"),]           -> y_b ; dim(b)
-  #  ds[which(ds$pullURL=="uk.yahoo.com"),]        -> y_c ; dim(c)
-  #  ds[which(ds$pullURL=="uk.news.yahoo.com"),]   -> y_d ; dim(d)
-  #  ds[which(ds$pullURL=="sports.yahoo.com"),]    -> y_e ; dim(e)
-  #  dim(y_a)[1]+ dim(y_b)[1]+ dim(y_c)[1]+dim(y_d)[1]+dim(y_e)[1]
-
-
-
-# exds_export -> exds
-# write.csv(exds_export,"~/gayagenda/datasets/ex_kws.csv")
-# exds %>% group_by(keyword,quarter,year) %>% arrange(desc(year)) %>% summarise(n=n()) -> summed
