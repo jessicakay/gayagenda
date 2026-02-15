c4fe04699ac2347f2fbe7ce66f4dcce2ab1a4e9a
jess <jkant@bu.edu>
2026-02-15 15:32:46 -0500

    pull top 20

--- data cleaning/subset_scripts.R
+++ data cleaning/subset_scripts.R
@@ -30,3 +30,37 @@ mega_ds %>%
                    )) %>% 
     select(month, year, type, EntryTitle,EntryURL,pullURL,cat)  
     
+  mega_ds %>% filter(pullURL=="foxnews.com") %>% 
+    distinct(EntryURL, .keep_all = T) %>% 
+    filter(year == "2026") %>%
+    select(the_day,dayweek,month, quarter,year,EntryTitle,EntryURL,keyword,pullURL) %>%
+    mutate(type=case_when
+           (
+             str_detect(EntryURL,"/video/") == TRUE ~ "video",
+             str_detect(EntryURL,"/video/") == FALSE ~ "article"
+           )) %>% 
+    mutate(cat=gsub("/","",str_extract(EntryURL,pattern="/[a-z-]+/"))) %>%
+    select(month,cat) %>% table()
+           
+  
+ 
+search_query_1 = "Dr. Upton|Beth Upton|Dr. Beth Upton|transgender medic|trans* medic"
+search_query_2 = "trans|medic|"
+mega_ds %>%
+mutate(mention= 
+  case_when(
+    str_detect(EntryContent, pattern=search_query )==TRUE ~ str_extract(EntryContent,search_query)
+  )) -> ds_query
+
+ds_query %>%
+  group_by(mention, pullURL) %>%
+  count(mention,pullURL,.drop =T) 
+
+mega_ds %>%
+  mutate(mention= 
+           case_when(
+             str_detect(EntryContent, pattern=search_query )==TRUE ~ str_extract(EntryContent,search_query)
+           )) %>% 
+  group_by(mention, pullURL) %>%
+  count(mention,pullURL) #
+%>% select(pullURL,mention) %>% prop.table() 
