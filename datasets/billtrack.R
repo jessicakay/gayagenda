8471aa3ef62fb8fae8900fbd970ce62f9c289e09
jess <jkant@bu.edu>
2026-02-16 13:55:12 -0500

    fingers crossed

---
+++ datasets/billtrack.R
@@ -0,0 +1,10 @@
+
+
+
+if("installr" %in% installed.packages()==FALSE){
+  install.packages("airtable")} else {
+    installr::install.R()}
+
+if("rairtable" %in% installed.packages()==FALSE){
+  install.packages("rairtable")} else {
+    library(rairtable)}
