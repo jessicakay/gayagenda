e3a88c8406d2179adefcde9b0e9eb64fb360abeb
jess <jkant@bu.edu>
2026-03-13 19:13:03 -0400

    warning: explicit

--- datasets/subsets/youtube/yt.R
+++ datasets/subsets/youtube/yt.R
@@ -1,5 +1,6 @@
 
-  #   filter(region=="all regions"|is.na(region)) %>%
+
+# note: unspecified and all regions is merged, as these are functionally the same.
 
  youtube2023$region[is.na(youtube2023$region)]<-"all regions"
 
@@ -42,7 +43,7 @@ mega_ds %>% filter(pullURL=="youtube.com") %>%
 
 mega_ds %>% distinct(EntryURL) |> dim() -> b
 
-mega_ds %>% distinct(EntryURL,.keep_all = T) %>% select(pullURL) |> table() %>% View()
+mega_ds %>% distinct(EntryURL,.keep_all= T) %>% select(pullURL) |> table() %>% View()
 
 youtube2023 %>% distinct(EntryURL,.keep_all = T) %>% 
   filter(keyword=="transgender")%>%
@@ -51,7 +52,7 @@ youtube2023 %>% distinct(EntryURL,.keep_all = T) %>%
 
 
 
-c("EntryPublished","the_day","year","month","video_ID",
+c("EntryPublished","video_ID",
   "posted_on_YT","channel name","yt_long_ID","EntryTitle",
   "EntryURL","keyword","region","RSS") -> pull_columns
 
@@ -61,3 +62,89 @@ youtube2024 %>% select(any_of(pull_columns)),
 youtube2025 %>% select(any_of(pull_columns)))->yt_2023to2025
 
 write.csv(yt_2023to2025,"~/gayagenda/datasets/subsets/youtube/youtube_2023to2025_merged.csv")
+
+yt_2023to2025 %>%
+  mutate(the_day=as.Date(mdy(str_extract(
+    EntryPublished,pattern = "[a-zA-Z]+\\s[0-9]+\\,\\s20[0-9]+")))) %>%
+  mutate(month=month(lubridate::as_date(the_day))) %>%
+  mutate(dayweek=weekdays(the_day)) %>%
+  mutate(year = year(lubridate::as_date(the_day))) %>%
+  mutate(quarter = quarter(lubridate::as_date(the_day))) %>%
+  mutate(ym=paste0(month,"-",year)) %>%
+  arrange(the_day)-> yt_df
+
+# checking my work
+
+yt_2023to2025 %>% select(region,keyword) |> table() # why is UK missing? 
+
+mega_ds %>% filter(pullURL=="youtube.com") %>%   # UK never served YT as news??
+  select(region) |> table()                      # returns 5207 for all regions, 4505 for US
+                                                                        
+mega_ds %>% filter(pullURL=="youtube.com") %>% select(EntryURL) |> unique() |> dim() 
+
+yt_2023to2025 %>%
+  filter(region=="all regions") |> dim() # 5623 entries
+
+
+# plots
+
+yt_df
+
+yt_df %>%
+  filter(region=="all regions")%>%
+  filter(keyword %in% c("biological sex","gender identity","transgender")) %>%
+  mutate(year=as.factor(year))%>%
+  ggplot()+
+  theme_dark()+
+  geom_bar(aes(x=ym,fill=year),position = position_dodge(preserve = "single"))+
+  theme(plot.background=element_rect("black", colour = "black"),panel.grid = element_line("black"),  
+        panel.background = element_rect("black"),legend.background = element_rect("black"),
+        legend.box.background = element_rect("black"),legend.key = element_rect("black"),
+        text = element_text(colour = "white"),
+        legend.position = "bottom")+
+  scale_fill_paletteer_d("yarrr::info")+
+  facet_grid(.~keyword) 
+
+  core+geom_bar(aes(x=yq,fill=year),position = position_dodge(preserve = "single"))+
+    theme(plot.background=element_rect("black", colour = "black"),panel.grid = element_line("black"),  
+          panel.background = element_rect("black"),legend.background = element_rect("black"),
+          legend.box.background = element_rect("black"),legend.key = element_rect("black"),
+          text = element_text(colour = "white"),
+          legend.position = "bottom")+
+    scale_fill_paletteer_d("yarrr::info")+
+    facet_grid(.~keyword)
+
+  grid.arrange(plot_a,plot_b,ncol=2)  
+  
+  
+  yt_df %>%
+    filter(region=="all regions")%>%
+    filter(keyword %in% c("biological sex","gender identity","transgender")) %>%
+    mutate(year=as.factor(year))%>%
+    ggplot()+
+    theme_dark()+
+    geom_bar(aes(x=year,fill=year),position = position_dodge(preserve = "single"))+
+    theme(plot.background=element_rect("black", colour = "black"),panel.grid = element_line("black"),  
+          panel.background = element_rect("black"),legend.background = element_rect("black"),
+          legend.box.background = element_rect("black"),legend.key = element_rect("black"),
+          text = element_text(colour = "white"),
+          legend.position = "bottom")+
+    scale_fill_paletteer_d("yarrr::info")+
+      facet_grid(.~keyword)-> simple_year
+  
+  
+  yt_df %>%
+    filter(region=="all regions")%>%
+    filter(keyword %in% c("biological sex","gender identity","transgender")) %>%
+    mutate(year=as.factor(year))%>%
+    ggplot()+
+    theme_dark()+
+    geom_bar(aes(x=year,fill=as.factor(month)),position = position_dodge(preserve = "single"))+
+    theme(plot.background=element_rect("black", colour = "black"),panel.grid = element_line("black"),  
+          panel.background = element_rect("black"),legend.background = element_rect("black"),
+          legend.box.background = element_rect("black"),legend.key = element_rect("black"),
+          text = element_text(colour = "white"),
+          legend.position = "bottom")+
+    scale_fill_paletteer_d("beyonce::X98")+
+    facet_grid(.~keyword)
+  
